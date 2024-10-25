3cf0d337e2ad769311de7039cc718e5b85b69d30
Paweł Małolepszy <pawel.malolepszy@gmail.com>
2024-10-25 23:47:50 +0200

    chore(nvim): move autocmd to init

--- dot_config/nvim/lua/phpeek/init.lua
+++ dot_config/nvim/lua/phpeek/init.lua
@@ -124,5 +124,18 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('', 'H', '^')
 vim.keymap.set('', 'L', '$')
 
-require 'phpeek.autocmd'
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 require 'phpeek.lazy'
