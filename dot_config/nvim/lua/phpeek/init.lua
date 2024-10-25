7a9ae0089dd2574c68be99e18ae38d1055b5bb33
Paweł Małolepszy <pawel.malolepszy@gmail.com>
2024-10-26 00:19:27 +0200

    chore(nvim): move gitsigns to init

--- dot_config/nvim/lua/phpeek/init.lua
+++ dot_config/nvim/lua/phpeek/init.lua
@@ -162,6 +162,19 @@ require('lazy').setup {
 
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+
   spec = {
     -- import your plugins
     { import = 'phpeek.plugins' },
