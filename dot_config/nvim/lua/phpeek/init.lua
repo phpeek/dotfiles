96413c9c5a355074ffb32b3d10f3c9eda6870398
Paweł Małolepszy <pawel.malolepszy@gmail.com>
2024-12-09 08:09:03 +0100

    chore(nvim): disable pylint and mypy

--- dot_config/nvim/lua/phpeek/init.lua
+++ dot_config/nvim/lua/phpeek/init.lua
@@ -1061,7 +1061,7 @@ require('lazy').setup({
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
-        python = { 'pylint', 'mypy' },
+        -- python = { 'pylint', 'mypy' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
