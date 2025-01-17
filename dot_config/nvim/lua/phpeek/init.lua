47f60c197473317aef71e9e24eb87d1607c0eab6
Paweł Małolepszy <pawel.malolepszy@gmail.com>
2025-01-17 20:52:33 +0100

    feat(nvim): use prettierd formatter on json

--- dot_config/nvim/lua/phpeek/init.lua
+++ dot_config/nvim/lua/phpeek/init.lua
@@ -290,7 +290,7 @@ require('lazy').setup({
     'ibhagwan/fzf-lua',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     cmd = 'FzfLua',
-    opts = function(_, opts)
+    opts = function(_, _)
       local config = require 'fzf-lua.config'
 
       -- send items to quickfix list
@@ -599,6 +599,7 @@ require('lazy').setup({
         html = { 'prettierd' },
         javascriptreact = { 'prettierd' },
         typescriptreact = { 'prettierd' },
+        json = { 'prettierd' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
@@ -628,12 +629,12 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
       },
       'saadparwaiz1/cmp_luasnip',
